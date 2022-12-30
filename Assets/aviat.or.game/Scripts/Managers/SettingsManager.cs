8e88d221980bd2c11c0584f6b35eac5a4096dec7
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-31 02:14:20 +0300

    update

--- Assets/aviat.or.game/Scripts/Managers/SettingsManager.cs
+++ Assets/aviat.or.game/Scripts/Managers/SettingsManager.cs
@@ -11,11 +11,6 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] AudioSource sfx;
     [SerializeField] Button sfxBtn;
 
-    [Space(10)]
-    [SerializeField] Button vibroBtn;
-
-    public static bool VibraEnable { get; set; } = false;
-
     private void Start()
     {
         loop.mute = sfx.mute = true;
@@ -23,24 +18,21 @@ public class SettingsManager : MonoBehaviour
         soundBtn.onClick.AddListener(() =>
         {
             loop.mute = !loop.mute;
+
             soundBtn.transform.GetChild(0).gameObject.SetActive(!loop.mute);
+            soundBtn.transform.GetChild(1).gameObject.SetActive(loop.mute);
         });
 
 
         sfxBtn.onClick.AddListener(() =>
         {
             sfx.mute = !sfx.mute;
-            sfxBtn.transform.GetChild(0).gameObject.SetActive(!sfx.mute);
-        });
 
-        vibroBtn.onClick.AddListener(() =>
-        {
-            VibraEnable = !VibraEnable;
-            vibroBtn.transform.GetChild(0).gameObject.SetActive(VibraEnable);
+            sfxBtn.transform.GetChild(0).gameObject.SetActive(!sfx.mute);
+            sfxBtn.transform.GetChild(1).gameObject.SetActive(sfx.mute);
         });
 
         soundBtn.onClick.Invoke();
         sfxBtn.onClick.Invoke();
-        vibroBtn.onClick.Invoke();
     }
 }
